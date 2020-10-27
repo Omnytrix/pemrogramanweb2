de9ae6ad8a4aacf7d0d788f9ee1a9fe96a5edafa
Omnytrix <cz2licious.hf@gmail.com>
2020-10-27 14:00:17 +0800

    Praktikum Bootstrap 27/10/2020

--- application/controllers/mahasiswa.php
+++ application/controllers/mahasiswa.php
@@ -1,6 +1,15 @@
 <?php
 class mahasiswa extends CI_controller
 {
+	public function index($url_nim = '', $url_jur = '')
+	{
+		$data['nama'] = 'Himawan Fitriadi';
+		$data['umur'] = '35 tahun';
+		$data['nim'] = $url_nim;
+		$data['jur'] = $url_jur;
+
+		$this->load->view('mahasiswa/v_index', $data);
+	}
 	public function jurusan()
 	{
 		echo "D3 Sistem Informasi";
---
+++ application/views/mahasiswa/v_index.php
@@ -0,0 +1,21 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Data Mahasiswa</title>
+	<link rel="stylesheet"
+		  href="<?=base_url('assets/bootstrap/css/bootstrap.min.css');?>">
+
+</head>
+<body>
+
+<h2>Data Mahasiswa</h2>
+<p> Nama : <?php echo $nama;?></p>
+<p> Umur : <?php echo $umur;?></p>
+<p> NIM  : <?php echo $nim;?></p>
+<p> Jurusan  : <?php echo $jur;?></p>
+
+</body>
+</html>
